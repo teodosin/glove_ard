50c0f4db9a56f96bace61a911cc532528981e60d
Viktor Teodosin <teodosin@protonmail.com>
2024-05-18 11:42:29 +0300

    yes

---
+++ read_flex/bleuart.ino
@@ -0,0 +1,36 @@
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  flex_setup();
+  ble_setup();
+}
+
+
+void loop()
+{
+  // Forward data from HW Serial to BLEUART
+  while (Serial.available())
+  {
+    // Delay to wait for enough input, since we have a limited transmission buffer
+    delay(2);
+
+
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    bleuart.write( buf, count );
+  }
+
+  // Forward from BLEUART to HW Serial
+  while ( bleuart.available() )
+  {
+    uint8_t ch;
+    ch = (uint8_t) bleuart.read();
+    Serial.write(ch);
+  }
+  
+  flex_loop();
+}
+
--- read_flex/read_flex.ino
+++ read_flex/read_flex.ino
@@ -6,25 +6,68 @@ const int s2 = D0;
 const int s1 = D1;
 const int s0 = D2; 
 
-void setup() {
+void flex_setup() {
   
   pinMode(flx, INPUT);  // declare the sensorPin as an INPUT
   pinMode(s2, OUTPUT);   // declare the ledPin as an OUTPUT
   pinMode(s1, OUTPUT);
   pinMode(s0, OUTPUT);
 
-  digitalWrite(s2, HIGH);
-  digitalWrite(s1, LOW);
-  digitalWrite(s0, LOW);
-
+  set_selector(1, s0, s1, s2);
 
   Serial.begin(115200);
   while (!Serial);
 }
 
-void loop() {
+void flex_loop() {
   // read the value from the sensor:
   int sensorValue = analogRead(flx);
 
   Serial.println(sensorValue);
 }
+
+void set_selector(int pin, int s0, int s1, int s2) {
+  switch (pin) {
+    case 0: // A0
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+    case 1: // A1
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, HIGH);
+      break;
+    case 2: // A2
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, LOW);
+      break;
+    case 3: // A3
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, HIGH);
+      break;
+    case 4: // A4
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+    case 5: // A5
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, HIGH);
+      break;
+    case 6: // A6
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, LOW);
+      break;
+    case 7: // A7
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, HIGH);
+      break;
+  }
+}
+
